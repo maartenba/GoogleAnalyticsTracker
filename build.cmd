26649a4643a04ff78e6cce793911098e3e9141a3
Maarten Balliauw <maarten@maartenballiauw.be>
2012-05-30 13:00:30 +0200

    Updated build scripts for WP71 build

--- build.cmd
+++ build.cmd
@@ -4,8 +4,15 @@ if "%config%" == "" (
    set config=Debug
 )
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild GoogleAnalyticsTracker.WindowsPhone\GoogleAnalyticsTracker.WindowsPhone.csproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 mkdir Build
-copy GoogleAnalyticsTracker\bin\%Config%\*.nupkg Build
-copy GoogleAnalyticsTracker\bin\%Config%\*.dll Build
-copy GoogleAnalyticsTracker\bin\%Config%\*.pdb Build
+mkdir Build\net40
+copy GoogleAnalyticsTracker\bin\%Config%\*.nupkg Build\net40
+copy GoogleAnalyticsTracker\bin\%Config%\*.dll Build\net40
+copy GoogleAnalyticsTracker\bin\%Config%\*.pdb Build\net40
+
+mkdir Build\sl4-wp71
+copy GoogleAnalyticsTracker.WindowsPhone\bin\%Config%\*.nupkg Build\sl4-wp71
+copy GoogleAnalyticsTracker.WindowsPhone\bin\%Config%\*.dll Build\sl4-wp71
+copy GoogleAnalyticsTracker.WindowsPhone\bin\%Config%\*.pdb Build\sl4-wp71
