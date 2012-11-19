71eece639471cc7e822b02dfe5bee34d5fa44548
Maarten Balliauw <maarten@balliauw.be>
2012-11-19 13:46:43 +0100

    Formatted Placeholder.cs
    Formatted Placeholder.cs

--- GoogleAnalyticsTracker.RT/Interop/Placeholder.cs
+++ GoogleAnalyticsTracker.RT/Interop/Placeholder.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 namespace System.Configuration
 {
     /// <summary>
-    /// This only exists so the System.Configuration namespace exists so I don't
-    /// have to #ifdef it out
+    /// This only exists so the System.Configuration namespace exists so I don't have to #ifdef it out
     /// </summary>
     internal sealed class Placeholder
     { 
