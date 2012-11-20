1cc36266a23349a38756662c8ae7e92d38155ad8
Maarten Balliauw <maarten@balliauw.be>
2012-11-20 09:11:44 +0100

    Issue #17 - Allow for 'awaiting' Tracker methods
    Issue #17 - Allow for 'awaiting' Tracker methods

---
+++ GoogleAnalyticsTracker/Tracker.tracking.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+
+namespace GoogleAnalyticsTracker
+{
+    public partial class Tracker
+    {
+        public void TrackPageView(string pageTitle, string pageUrl)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("AnalyticsVersion", AnalyticsVersion);
+            parameters.Add("utmn", GenerateUtmn());
+            parameters.Add("utmhn", Hostname);
+            parameters.Add("utmcs", CharacterSet);
+            parameters.Add("utmul", Language);
+            parameters.Add("utmdt", pageTitle);
+            parameters.Add("utmhid", AnalyticsSession.GenerateSessionId());
+            parameters.Add("utmp", pageUrl);
+            parameters.Add("utmac", TrackingAccount);
+            parameters.Add("utmcc", AnalyticsSession.GenerateCookieValue());
+
+            var utme = _utmeGenerator.Generate();
+            if (!string.IsNullOrEmpty(utme))
+                parameters.Add("utme", utme);
+
+            RequestUrlAsync(UseSsl ? BeaconUrlSsl : BeaconUrl, parameters);
+        }
+
+        public void TrackEvent(string category, string action, string label, int value)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("AnalyticsVersion", AnalyticsVersion);
+            parameters.Add("utmn", GenerateUtmn());
+            parameters.Add("utmhn", Hostname);
+            parameters.Add("utmni", "1");
+            parameters.Add("utmt", "event");
+
+            var utme = _utmeGenerator.Generate();
+            parameters.Add("utme", string.Format("5({0}*{1}*{2})({3})", category, action, label ?? "", value) + utme);
+
+            parameters.Add("utmcs", CharacterSet);
+            parameters.Add("utmul", Language);
+            parameters.Add("utmhid", AnalyticsSession.GenerateSessionId());
+            parameters.Add("utmac", TrackingAccount);
+            parameters.Add("utmcc", AnalyticsSession.GenerateCookieValue());
+
+            RequestUrlAsync(UseSsl ? BeaconUrlSsl : BeaconUrl, parameters);
+        }
+
+        public void TrackTransaction(string orderId, string storeName, string total, string tax, string shipping, string city, string region, string country)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("AnalyticsVersion", AnalyticsVersion);
+            parameters.Add("utmn", GenerateUtmn());
+            parameters.Add("utmhn", Hostname);
+            parameters.Add("utmt", "event");
+            parameters.Add("utmcs", CharacterSet);
+            parameters.Add("utmul", Language);
+            parameters.Add("utmhid", AnalyticsSession.GenerateSessionId());
+            parameters.Add("utmac", TrackingAccount);
+            parameters.Add("utmcc", AnalyticsSession.GenerateCookieValue());
+
+            parameters.Add("utmtid", orderId);
+            parameters.Add("utmtst", storeName);
+            parameters.Add("utmtto", total);
+            parameters.Add("utmttx", tax);
+            parameters.Add("utmtsp", shipping);
+            parameters.Add("utmtci", city);
+            parameters.Add("utmtrg", region);
+            parameters.Add("utmtco", country);
+
+            RequestUrlAsync(UseSsl ? BeaconUrlSsl : BeaconUrl, parameters);
+        }
+    }
+}
