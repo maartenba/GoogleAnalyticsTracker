7cea27e543899329b6735851bc06cc627643ffed
Maarten Balliauw <maarten@balliauw.be>
2013-01-05 21:19:20 +0100

    Small fix for solutiondir in build script
    Small fix for solutiondir in build script

--- build.bat
+++ build.bat
@@ -8,16 +8,16 @@ if "%config%" == "" (
 
 mkdir Build
 mkdir Build\net40
-.nuget\nuget.exe pack "GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj" -symbols -o Build\net40
+.nuget\nuget.exe pack "GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj" -symbols -o Build\net40 -p SolutionDir=%cd%
 copy GoogleAnalyticsTracker\bin\%Config%\*.dll Build\net40
 copy GoogleAnalyticsTracker\bin\%Config%\*.pdb Build\net40
 
 mkdir Build\sl4-wp71
-.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP7\GoogleAnalyticsTracker.WP7.csproj" -symbols -o Build\sl4-wp71
+.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP7\GoogleAnalyticsTracker.WP7.csproj" -symbols -o Build\sl4-wp71 -p SolutionDir=%cd%
 copy GoogleAnalyticsTracker.WP7\bin\%Config%\*.dll Build\sl4-wp71
 copy GoogleAnalyticsTracker.WP7\bin\%Config%\*.pdb Build\sl4-wp71
 
 mkdir Build\wp8
-.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\wp8
+.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\wp8 -p SolutionDir=%cd%
 copy GoogleAnalyticsTracker.WP8\bin\%Config%\*.dll Build\wp8
 copy GoogleAnalyticsTracker.WP8\bin\%Config%\*.pdb Build\wp8
