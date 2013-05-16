55027ad9f7922970bd32e730b339ad1242527579
Maarten Balliauw <maarten@balliauw.be>
2013-05-16 09:55:15 +0200

    Updated build script
    Updated build script

--- build.bat
+++ build.bat
@@ -13,16 +13,16 @@ if not "%PackageVersion%" == "" (
 
 mkdir Build
 mkdir Build\net40
-.nuget\nuget.exe pack "GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj" -symbols -o Build\net40 -p SolutionDir=%cd% %version%
+.nuget\nuget.exe pack "GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj" -symbols -o Build\net40 -p SolutionDir=%cd% -p Configuration=%config% %version%
 copy GoogleAnalyticsTracker\bin\%config%\*.dll Build\net40
 copy GoogleAnalyticsTracker\bin\%config%\*.pdb Build\net40
 
 mkdir Build\sl4-wp71
-.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP7\GoogleAnalyticsTracker.WP7.csproj" -symbols -o Build\sl4-wp71 -p SolutionDir=%cd% %version%
+.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP7\GoogleAnalyticsTracker.WP7.csproj" -symbols -o Build\sl4-wp71 -p SolutionDir=%cd% -p Configuration=%config% %version%
 copy GoogleAnalyticsTracker.WP7\bin\%config%\*.dll Build\sl4-wp71
 copy GoogleAnalyticsTracker.WP7\bin\%config%\*.pdb Build\sl4-wp71
 
 mkdir Build\wp8
-.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\wp8 -p SolutionDir=%cd% %version%
+.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\wp8 -p SolutionDir=%cd% -p Configuration=%config% %version%
 copy GoogleAnalyticsTracker.WP8\bin\%config%\*.dll Build\wp8
 copy GoogleAnalyticsTracker.WP8\bin\%config%\*.pdb Build\wp8
