ec77e8b7f3ab5c5a20f565c7b2a4b868c4b06194
Maarten Balliauw <maarten@balliauw.be>
2013-05-16 10:30:13 +0200

    Updated build process
    Updated build process

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo Off
-set configuration=%1
-if "%configuration%" == "" (
-   set configuration=Release
+set config=%1
+if "%config%" == "" (
+   set config=Release
 )
 
 set version=
@@ -9,20 +9,20 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild GoogleAnalyticsTracker.sln /p:Configuration="%configuration%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild GoogleAnalyticsTracker.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 mkdir Build
 mkdir Build\net40
-.nuget\nuget.exe pack "GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj" -symbols -o Build\net40 -p SolutionDir=%cd% -p Configuration=%configuration% %version%
-copy GoogleAnalyticsTracker\bin\%configuration%\*.dll Build\net40
-copy GoogleAnalyticsTracker\bin\%configuration%\*.pdb Build\net40
+.nuget\nuget.exe pack "GoogleAnalyticsTracker\GoogleAnalyticsTracker.csproj" -symbols -o Build\net40 -p Configuration=%config% %version%
+copy GoogleAnalyticsTracker\bin\%config%\*.dll Build\net40
+copy GoogleAnalyticsTracker\bin\%config%\*.pdb Build\net40
 
 mkdir Build\sl4-wp71
-.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP7\GoogleAnalyticsTracker.WP7.csproj" -symbols -o Build\sl4-wp71 -p SolutionDir=%cd% -p Configuration=%configuration% %version%
-copy GoogleAnalyticsTracker.WP7\bin\%configuration%\*.dll Build\sl4-wp71
-copy GoogleAnalyticsTracker.WP7\bin\%configuration%\*.pdb Build\sl4-wp71
+.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP7\GoogleAnalyticsTracker.WP7.csproj" -symbols -o Build\sl4-wp71 -p Configuration=%config% %version%
+copy GoogleAnalyticsTracker.WP7\bin\%config%\*.dll Build\sl4-wp71
+copy GoogleAnalyticsTracker.WP7\bin\%config%\*.pdb Build\sl4-wp71
 
 mkdir Build\wp8
-.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\wp8 -p SolutionDir=%cd% -p Configuration=%configuration% %version%
-copy GoogleAnalyticsTracker.WP8\bin\%configuration%\*.dll Build\wp8
-copy GoogleAnalyticsTracker.WP8\bin\%configuration%\*.pdb Build\wp8
+.nuget\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\wp8 -p Configuration=%config% %version%
+copy GoogleAnalyticsTracker.WP8\bin\%config%\*.dll Build\wp8
+copy GoogleAnalyticsTracker.WP8\bin\%config%\*.pdb Build\wp8
