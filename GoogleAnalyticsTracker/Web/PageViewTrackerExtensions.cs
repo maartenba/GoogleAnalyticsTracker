bd4d7a0285ff7a3ef58ede60c9711d88b1a84615
Thierry Malo <tmalo@plasmasoft.fr>
2013-06-06 16:51:14 +0200

    New Api ActionTrackingAttribute
    an ActionTrackingAttribute to use in MVC API project

--- GoogleAnalyticsTracker/Web/PageViewTrackerExtensions.cs
+++ GoogleAnalyticsTracker/Web/PageViewTrackerExtensions.cs
@@ -19,5 +19,13 @@ namespace GoogleAnalyticsTracker
             tracker.Language = request.UserLanguages != null ? string.Join(";", request.UserLanguages) : "";
             tracker.TrackPageView(pageTitle, pageUrl);
         }
+
+        public static void TrackPageView(this Tracker tracker, System.Net.Http.HttpRequestMessage request , string pageTitle, string pageUrl)
+        {
+            tracker.Hostname = request.Headers.From;
+            tracker.UserAgent = request.Headers.UserAgent != null ? string.Join(";", request.Headers.UserAgent) : "";
+            tracker.Language = request.Headers.AcceptLanguage != null ? string.Join(";", request.Headers.AcceptLanguage) : "";
+            tracker.TrackPageView(pageTitle, pageUrl);
+        }
     }
 }
