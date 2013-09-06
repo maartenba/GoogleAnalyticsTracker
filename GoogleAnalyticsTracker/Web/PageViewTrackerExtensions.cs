4beb053cdd8b5011d268a8fbfc4e028a254d2987
Eric Falsken <eric@everylittlething.net>
2013-09-06 11:56:38 -0700

    Moved WebAPI-related stuff to separate assembly.

--- GoogleAnalyticsTracker/Web/PageViewTrackerExtensions.cs
+++ GoogleAnalyticsTracker/Web/PageViewTrackerExtensions.cs
@@ -1,39 +1,10 @@
 using System;
 using System.Web;
-using System.Net.Http;
 
 // ReSharper disable CheckNamespace
 namespace GoogleAnalyticsTracker{
 	// ReSharper restore CheckNamespace
 	public static class PageViewTrackerExtensions {
-
-		#region WebAPI extensions
-		public static void TrackPageView(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle) {
-			TrackPageView(tracker, httpRequest, pageTitle, httpRequest.RequestUri.PathAndQuery);
-		}
-
-		public static void TrackPageView(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle, string pageUrl) {
-			tracker.TrackPageView(pageTitle, pageUrl,
-				hostname: httpRequest.RequestUri.Host,
-				userAgent: httpRequest.Headers.UserAgent.ToString(),
-				language: httpRequest.Headers.AcceptLanguage.ToString()
-			);
-		}
-
-		public static void TrackPageViewAsync(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle) {
-			TrackPageViewAsync(tracker, httpRequest, pageTitle, httpRequest.RequestUri.PathAndQuery);
-		}
-
-		public static void TrackPageViewAsync(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle, string pageUrl) {
-			tracker.TrackPageViewAsync(pageTitle, pageUrl,
-				hostname: httpRequest.RequestUri.Host,
-				userAgent: httpRequest.Headers.UserAgent.ToString(),
-				language: httpRequest.Headers.AcceptLanguage.ToString()
-			);
-		}
-		#endregion
-
-		#region ASP.NET Classic extensions
 		public static void TrackPageView(this Tracker tracker, HttpContextBase httpContext, string pageTitle) {
 			TrackPageView(tracker, httpContext, pageTitle, httpContext.Request.Url.PathAndQuery);
 		}
@@ -59,6 +30,5 @@ namespace GoogleAnalyticsTracker{
 				language: request.UserLanguages != null ? string.Join(";", request.UserLanguages) : ""
 			);
 		}
-		#endregion
 	}
 }
