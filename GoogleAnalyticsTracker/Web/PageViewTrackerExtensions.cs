1ab0dfddb8a60d2b92cfcfcee509c0dc3fc9bd48
Eric Falsken <eric@everylittlething.net>
2013-09-06 11:48:25 -0700

    Added support for ASP.NET WebAPI.

--- GoogleAnalyticsTracker/Web/PageViewTrackerExtensions.cs
+++ GoogleAnalyticsTracker/Web/PageViewTrackerExtensions.cs
@@ -1,12 +1,39 @@
 using System;
 using System.Web;
+using System.Net.Http;
 
 // ReSharper disable CheckNamespace
-namespace GoogleAnalyticsTracker
+namespace GoogleAnalyticsTracker{
 	// ReSharper restore CheckNamespace
-{
 	public static class PageViewTrackerExtensions {
 
+		#region WebAPI extensions
+		public static void TrackPageView(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle) {
+			TrackPageView(tracker, httpRequest, pageTitle, httpRequest.RequestUri.PathAndQuery);
+		}
+
+		public static void TrackPageView(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle, string pageUrl) {
+			tracker.TrackPageView(pageTitle, pageUrl,
+				hostname: httpRequest.RequestUri.Host,
+				userAgent: httpRequest.Headers.UserAgent.ToString(),
+				language: httpRequest.Headers.AcceptLanguage.ToString()
+			);
+		}
+
+		public static void TrackPageViewAsync(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle) {
+			TrackPageViewAsync(tracker, httpRequest, pageTitle, httpRequest.RequestUri.PathAndQuery);
+		}
+
+		public static void TrackPageViewAsync(this Tracker tracker, HttpRequestMessage httpRequest, string pageTitle, string pageUrl) {
+			tracker.TrackPageViewAsync(pageTitle, pageUrl,
+				hostname: httpRequest.RequestUri.Host,
+				userAgent: httpRequest.Headers.UserAgent.ToString(),
+				language: httpRequest.Headers.AcceptLanguage.ToString()
+			);
+		}
+		#endregion
+
+		#region ASP.NET Classic extensions
 		public static void TrackPageView(this Tracker tracker, HttpContextBase httpContext, string pageTitle) {
 			TrackPageView(tracker, httpContext, pageTitle, httpContext.Request.Url.PathAndQuery);
 		}
@@ -14,8 +41,8 @@ namespace GoogleAnalyticsTracker
 		public static void TrackPageView(this Tracker tracker, HttpContextBase httpContext, string pageTitle, string pageUrl) {
 			var request = httpContext.Request;
 			tracker.TrackPageView(pageTitle, pageUrl,
-				hostname: request.Url.Host, 
-				userAgent: request.UserAgent, 
+				hostname: request.Url.Host,
+				userAgent: request.UserAgent,
 				language: request.UserLanguages != null ? string.Join(";", request.UserLanguages) : ""
 			);
 		}
@@ -32,5 +59,6 @@ namespace GoogleAnalyticsTracker
 				language: request.UserLanguages != null ? string.Join(";", request.UserLanguages) : ""
 			);
 		}
+		#endregion
 	}
 }
