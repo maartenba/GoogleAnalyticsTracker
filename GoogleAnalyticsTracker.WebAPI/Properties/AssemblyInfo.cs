56258912e295be6f78a7849919068c1c6bfdd920
Maarten Balliauw <maarten@maartenballiauw.be>
2013-09-07 13:19:08 +0200

    More changes for build
    More changes for build

--- GoogleAnalyticsTracker.WebAPI/Properties/AssemblyInfo.cs
+++ GoogleAnalyticsTracker.WebAPI/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GoogleAnalyticsTracker.WebAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GoogleAnalyticsTracker for ASP.NET Web API - A C# library for tracking Google Analytics.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GoogleAnalyticsTracker.WebAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Maarten Balliauw")]
+[assembly: AssemblyProduct("GoogleAnalyticsTracker")]
+[assembly: AssemblyCopyright("Copyright © Maarten Balliauw 2012 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
