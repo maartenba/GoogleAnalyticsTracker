5516745d93e991932d7eb9c2df991958671d40c4
Maarten Balliauw <maarten@maartenballiauw.be>
2014-03-28 15:52:27 +0100

    Get rid of Microsoft.Bcl.Async

--- GoogleAnalyticsTracker.Core/ITrackerEnvironment.cs
+++ GoogleAnalyticsTracker.Core/ITrackerEnvironment.cs
@@ -2,7 +2,7 @@ namespace GoogleAnalyticsTracker.Core
 {
     public interface ITrackerEnvironment
     {
-        string Hostname { get; set; }
+        string Hostname { get; set; } 
         string OsPlatform { get; set; }
         string OsVersion { get; set; }
         string OsVersionString { get; set; }
