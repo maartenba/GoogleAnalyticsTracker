7ecb0b781ce9fe257c8dd108105652d08d5eb9d5
Carlos Henrique Gandarez <gandarez@gmail.com>
2015-05-13 14:07:55 -0300

    Fixed build.bat

--- build.bat
+++ build.bat
@@ -21,10 +21,9 @@ mkdir Build\nuget
 tools\nuget.exe pack "GoogleAnalyticsTracker.Core\GoogleAnalyticsTracker.Core.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
 tools\nuget.exe pack "GoogleAnalyticsTracker.Simple\GoogleAnalyticsTracker.Simple.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
 tools\nuget.exe pack "GoogleAnalyticsTracker.MVC4\GoogleAnalyticsTracker.MVC4.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
+tools\nuget.exe pack "GoogleAnalyticsTracker.MVC5\GoogleAnalyticsTracker.MVC5.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
 tools\nuget.exe pack "GoogleAnalyticsTracker.WebAPI\GoogleAnalyticsTracker.WebAPI.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
 tools\nuget.exe pack "GoogleAnalyticsTracker.WebAPI2\GoogleAnalyticsTracker.WebAPI2.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
-tools\nuget.exe pack "GoogleAnalyticsTracker.RT\GoogleAnalyticsTracker.RT.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
-tools\nuget.exe pack "GoogleAnalyticsTracker.WP8\GoogleAnalyticsTracker.WP8.csproj" -symbols -o Build\nuget -p Configuration=%config% %version%
 
 REM Plain assemblies
 mkdir Build\assemblies
@@ -34,11 +33,9 @@ copy GoogleAnalyticsTracker.Simple\bin\%config%\Google*.dll Build\assemblies
 copy GoogleAnalyticsTracker.Simple\bin\%config%\Google*.pdb Build\assemblies
 copy GoogleAnalyticsTracker.MVC4\bin\%config%\Google*.dll Build\assemblies
 copy GoogleAnalyticsTracker.MVC4\bin\%config%\Google*.pdb Build\assemblies
+copy GoogleAnalyticsTracker.MVC5\bin\%config%\Google*.dll Build\assemblies
+copy GoogleAnalyticsTracker.MVC5\bin\%config%\Google*.pdb Build\assemblies
 copy GoogleAnalyticsTracker.WebAPI\bin\%config%\Google*.dll Build\assemblies
 copy GoogleAnalyticsTracker.WebAPI\bin\%config%\Google*.pdb Build\assemblies
 copy GoogleAnalyticsTracker.WebAPI2\bin\%config%\Google*.dll Build\assemblies
 copy GoogleAnalyticsTracker.WebAPI2\bin\%config%\Google*.pdb Build\assemblies
-copy GoogleAnalyticsTracker.RT\bin\%config%\Google*.dll Build\assemblies
-copy GoogleAnalyticsTracker.RT\bin\%config%\Google*.pdb Build\assemblies
-copy GoogleAnalyticsTracker.WP8\bin\%config%\Google*.dll Build\assemblies
-copy GoogleAnalyticsTracker.WP8\bin\%config%\Google*.pdb Build\assemblies
