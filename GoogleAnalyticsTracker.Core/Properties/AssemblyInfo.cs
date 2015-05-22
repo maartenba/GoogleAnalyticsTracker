dc772c7238df55f6cc3a57101ebecd330beb6bf0
Maarten Balliauw <maarten@maartenballiauw.be>
2015-05-22 14:46:11 +0200

    Added WinRT and WP8 projects again.

--- GoogleAnalyticsTracker.Core/Properties/AssemblyInfo.cs
+++ GoogleAnalyticsTracker.Core/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GoogleAnalyticsTracker.Core")]
 [assembly: AssemblyDescription("GoogleAnalyticsTracker - A C# library for tracking Google Analytics.")]
-[assembly: Guid("105ceb96-8881-4409-b53b-aa695ecd0510")]
