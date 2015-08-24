849d70481d8bb6b7e346ff2411b7b5018bedddc8
Yu Sasaki <ysasaki.dev@gmail.com>
2015-08-24 10:43:19 -0700

    Added support for ContentExperiment

---
+++ GoogleAnalyticsTracker.Core/TrackerParameters/ContentExperiments.cs
@@ -0,0 +1,41 @@
+﻿using GoogleAnalyticsTracker.Core.TrackerParameters.Interface;
+
+namespace GoogleAnalyticsTracker.Core.TrackerParameters
+{
+    public class ContentExperiments : GeneralParameters, IContentExperimentsParameters
+    {
+        #region Overrides of GeneralParameters
+
+        /// <summary>
+        /// The type of hit. Must be one of 'pageview', 'screenview', 'event', 'transaction', 'item', 'social', 'exception', 'timing'.
+        /// <remarks>Required for all hit types</remarks>
+        /// <example>HitType.Pageview</example>
+        /// </summary>  
+        public override HitType HitType
+        {
+            get { return HitType.Pageview; }
+        }
+
+        #endregion
+
+        #region Implementation of IContentExperimentsParameters
+
+        /// <summary>
+        /// Specifies the experiment id.
+        /// <remarks>Required for experiment tracking.</remarks>
+        /// <example>K7Q-9lpLSd21prp9vIhdoA</example>
+        /// </summary>     
+        [Beacon("xid")]
+        public string ExperimentId { get; set; }
+
+        /// <summary>
+        /// Specifies the experiment variant id.
+        /// <remarks>Required for content experiment tracking.</remarks>
+        /// <example>1</example>
+        /// </summary>    
+        [Beacon("xvar")]
+        public string ExperimentVariant { get; set; }
+
+        #endregion
+    }
+}
