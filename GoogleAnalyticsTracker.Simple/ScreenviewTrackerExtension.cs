ca44b683e9468bb6f23cd15d8eeb6df9cb7cf536
FaMe IT <admin@fame-it.net>
2015-12-13 10:40:34 +0100

    Update ScreenviewTrackerExtension.cs
    Add screenName parameter; otherwise screenviewtracking is pretty useless.

--- GoogleAnalyticsTracker.Simple/ScreenviewTrackerExtension.cs
+++ GoogleAnalyticsTracker.Simple/ScreenviewTrackerExtension.cs
@@ -7,14 +7,15 @@ namespace GoogleAnalyticsTracker.Simple
     public static class ScreenviewTrackerExtension
     {
         public static async Task<TrackingResult> TrackScreenviewAsync(this SimpleTracker tracker, string appName,
-            string appId, string appVersion, string appInstallerId)
+            string appId, string appVersion, string appInstallerId, string screenName)
         {
             var screenviewParamenters = new ScreenviewTracking
             {
                 ApplicationName = appName,
                 ApplicationId = appId,
                 ApplicationVersion = appVersion,
-                ApplicationInstallerId = appInstallerId
+                ApplicationInstallerId = appInstallerId,
+                ScreenName =screenName
             };
 
             return await tracker.TrackAsync(screenviewParamenters);
