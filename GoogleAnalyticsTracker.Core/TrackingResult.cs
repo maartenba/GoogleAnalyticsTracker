757554ef7ffe9f5bc2a5fa5cb161f42ce1dbb208
Jonathan <vanillajonathan@users.noreply.github.com>
2016-05-09 16:30:54 +0200

    Added XML documentation for the Exception property

--- GoogleAnalyticsTracker.Core/TrackingResult.cs
+++ GoogleAnalyticsTracker.Core/TrackingResult.cs
@@ -5,9 +5,17 @@ namespace GoogleAnalyticsTracker.Core
 {
     public class TrackingResult
     {
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
         public string Url { get; set; }
+        
         public IDictionary<string, string> Parameters { get; set; }
         public bool Success { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the exception object.
+        /// </summary>
         public Exception Exception { get; set; }
     }
 }
