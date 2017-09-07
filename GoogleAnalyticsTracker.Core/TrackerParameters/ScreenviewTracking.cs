52432b99cf2107df201358a609f04af7ff5c921a
Ondra Medek <xmedeko@gmail.com>
2017-09-07 09:43:55 +0200

    Add IAppTrackingParameters and ICustomDimensionParameters to IGeneralParameters

--- GoogleAnalyticsTracker.Core/TrackerParameters/ScreenviewTracking.cs
+++ GoogleAnalyticsTracker.Core/TrackerParameters/ScreenviewTracking.cs
@@ -1,8 +1,6 @@
-using GoogleAnalyticsTracker.Core.TrackerParameters.Interface;
-
 namespace GoogleAnalyticsTracker.Core.TrackerParameters
 {
-    public class ScreenviewTracking : GeneralParameters, IAppTrackingParameters
+    public class ScreenviewTracking : GeneralParameters
     {
         #region Overrides of GeneralParameters
 
@@ -10,48 +8,12 @@ namespace GoogleAnalyticsTracker.Core.TrackerParameters
         /// The type of hit. Must be one of 'pageview', 'screenview', 'event', 'transaction', 'item', 'social', 'exception', 'timing'.
         /// <remarks>Required for all hit types</remarks>
         /// <example>HitType.Pageview</example>
-        /// </summary>  
+        /// </summary>
         public override HitType HitType
         {
             get { return HitType.Screenview; }
         }
 
-        #endregion
-
-        #region Implementation of IAppTrackingParameters
-
-        /// <summary>
-        /// Specifies the application name.
-        /// <remarks>Optional</remarks>
-        /// <example>My App</example>
-        /// </summary>
-        [Beacon("an")]
-        public string ApplicationName { get; set; }
-
-        /// <summary>
-        /// Application identifier.
-        /// <remarks>Optional</remarks>
-        /// <example>com.company.app</example>
-        /// </summary>
-        [Beacon("aid")]
-        public string ApplicationId { get; set; }
-
-        /// <summary>
-        /// Specifies the application version.
-        /// <remarks>Optional</remarks>
-        /// <example>1.2</example>
-        /// </summary>
-        [Beacon("av")]
-        public string ApplicationVersion { get; set; }
-
-        /// <summary>
-        /// Application installer identifier.
-        /// <remarks>Optional</remarks>
-        /// <example>com.platform.vending</example>
-        /// </summary>
-        [Beacon("aiid")]
-        public string ApplicationInstallerId { get; set; }
-
-        #endregion
+        #endregion Overrides of GeneralParameters
     }
 }
