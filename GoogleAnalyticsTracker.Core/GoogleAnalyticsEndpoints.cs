6454df85d8c172d483725cd14eb4fab0f9b37ea2
Maarten Balliauw <maarten@balliauw.be>
2018-07-02 15:59:06 +0200

    Update GoogleAnalyticsEndpoints.cs

--- GoogleAnalyticsTracker.Core/GoogleAnalyticsEndpoints.cs
+++ GoogleAnalyticsTracker.Core/GoogleAnalyticsEndpoints.cs
@@ -2,13 +2,14 @@ namespace GoogleAnalyticsTracker.Core
 {
     public static class GoogleAnalyticsEndpoints
     {
-        /// <summary> Default Google Endpoint URL. </summary>
+        /// <summary>Default Google Endpoint URL.</summary>
         public const string Default = "https://www.google-analytics.com/collect";
 
-        /// <summary> Non-secure Google Endpoint URL, not recommended. </summary>
+        /// <summary>Non-secure Google Endpoint URL, not recommended.</summary>
+        /// <remarks>It is not recommended to use this endpoint, as it uses no secure connection.</remarks>
         public const string NonSecure = "http://www.google-analytics.com/collect";
 
-        /// <summary> Debug, validating Google Endpoint. </summary>
+        /// <summary>Debug, validating Google Endpoint.</summary>
         public const string Debug = "https://www.google-analytics.com/debug/collect";
     }
 }
