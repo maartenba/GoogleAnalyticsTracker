c61e435d6062b34c2ea6bf107c0d31562ce36943
Kemal <kemaltaskin@yahoo.com>
2018-08-28 13:35:56 +0200

    Optional proxy settings.

---
+++ GoogleAnalyticsTracker.Core/WebProxy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace GoogleAnalyticsTracker.Core
+{
+    public class WebProxy : IWebProxy
+    {
+        private readonly Uri uri;
+
+        public WebProxy(string url, int port)
+        {
+            this.uri = new Uri($"{url}:{port}");
+        }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return uri;
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            return false;
+        }
+
+        public ICredentials Credentials { get; set; }
+    }
+}
