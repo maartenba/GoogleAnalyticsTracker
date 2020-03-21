16c555ad36de68f17d8a9bfc175a91ac07f1cc18
Adam Hwang <github@adamhwang.com>
2020-03-20 23:09:47 -0500

    Updated all projects to .Net Standard 2.0

--- GoogleAnalyticsTracker.Simple/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GoogleAnalyticsTracker.Simple")]
-[assembly: AssemblyDescription("GoogleAnalyticsTracker - A C# library for tracking Google Analytics.")]
-[assembly: Guid("B9334BB1-AC32-4F60-89B6-AC6709EC8CFA")]
