d7cb276eac8a7c11a4292361308f66aafe0d4bf6
Maarten Balliauw <maarten@maartenballiauw.be>
2020-03-22 17:37:44 +0100

    Drop older projects

--- oldGoogleAnalyticsTracker.WebAPI2/Interface/IRequireRequestAndResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Net.Http;
-
-namespace GoogleAnalyticsTracker.WebAPI2.Interface
-{
-    public interface IRequireRequestAndResponse
-    {
-        void SetRequestAndResponse(HttpRequestMessage requestMessage, HttpResponseMessage responseMessage);
-    }
-}
