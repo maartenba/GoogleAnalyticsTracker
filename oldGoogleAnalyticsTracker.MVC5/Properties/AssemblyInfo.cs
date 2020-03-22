356be4be8b45c5d688df93f44301d09c79289396
Maarten Balliauw <maarten@maartenballiauw.be>
2020-03-22 17:37:44 +0100

    Drop older projects

--- oldGoogleAnalyticsTracker.MVC5/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GoogleAnalyticsTracker.MVC5")]
-[assembly: AssemblyDescription("GoogleAnalyticsTracker - A C# library for tracking Google Analytics.")]
-[assembly: Guid("1b4262fa-b379-4b23-b396-da7ccecb3029")]
