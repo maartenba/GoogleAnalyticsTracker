1c410c4b2035f812465de28cc2e9298403fab523
Anton Negodyaev <anton.negodyaev@vmedia.ca>
2020-03-26 18:36:28 +0300

    Fix Enhanced transaction HitType -> Event

--- GoogleAnalyticsTracker.Core/TrackerParameters/EnhancedECommerceTransaction.cs
+++ GoogleAnalyticsTracker.Core/TrackerParameters/EnhancedECommerceTransaction.cs
@@ -12,6 +12,17 @@ namespace GoogleAnalyticsTracker.Core.TrackerParameters
             Products = new List<IEnhancedECommerceProduct>();
         }
 
+        #region Overrides of GeneralParameters
+
+        /// <summary>
+        /// The type of hit. Must be one of 'pageview', 'screenview', 'event', 'transaction', 'item', 'social', 'exception', 'timing'.
+        /// <remarks>Required for all hit types</remarks>
+        /// <example>HitType.Pageview</example>
+        /// </summary>  
+        public override HitType HitType => HitType.Event;
+
+        #endregion
+
         [Beacon("pa")]
         public ProductAction ProductAction { get; set; }
 
