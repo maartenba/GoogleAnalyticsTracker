71c6c0852f25993d5cbaf65c7447545231a47754
Anton Negodyaev <anton.negodyaev@vmedia.ca>
2020-03-26 12:42:45 +0300

    Implement Refund tracking

---
+++ GoogleAnalyticsTracker.Core/TrackerParameters/Interface/IRefundTrackingParameters.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace GoogleAnalyticsTracker.Core.TrackerParameters.Interface
+{
+    public interface IRefundTrackingParameters : IProvideProductsParameters
+    {
+        /// <summary>
+        /// Specifies the transaction Id to refund. Must not be empty.
+        /// <remarks>Required for refund</remarks>
+        /// <example>T12345</example>
+        /// </summary>
+        string TransactionId { get; set; }
+
+        /// <summary>
+        /// Specifies the product action. Must not be empty.
+        /// <remarks>Required for refund</remarks>
+        /// </summary>
+        ProductAction ProductAction { get; }
+
+        /// <summary>
+        /// Specifies the product list to refund.
+        /// <remarks>Optional</remarks>
+        /// </summary>
+        List<IEnhancedECommerceProduct> Products { get; set; }
+    }
+}
